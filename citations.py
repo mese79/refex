754fba055f700332788384ada9b0e7eeb930c5be
mese79 <mehdiseifi@gmail.com>
2022-09-12 21:10:19 +0430

    added querying refs by scholarly

--- citations.py
+++ citations.py
@@ -4,43 +4,43 @@ CITATIONS = {
     'Style1': {
         'name': 'Style1',
         'patterns': [
-            r'''(?P<authors>.+?)\.\s+(?P<title>.+?)\.\s+(?P<publish>.+?),\s+(?P<page>.+?),\s+(?P<year>19|20\d{2})\.''',
+            r'''(?P<authors>.+?)\.\s+(?P<title>.+?)\.\s+(?P<pub>.+?),\s+(?P<page>.+?),\s+(?P<year>19|20\d{2})\.''',
         ]
     },
     'MLA': {
         'name': 'Modern Language Association',
         'patterns': [
-            r'''(?P<authors>.+?\.)\s+"(?P<title>.+?)\."\s+(?P<publish>.+?)\s+\((?P<year>19|20\d{2})\)\.''',
+            r'''(?P<authors>.+?\.)\s+"(?P<title>.+?)\."\s+(?P<pub>.+?)\s+\((?P<year>19|20\d{2})\)\.''',
         ]
     },
     'Chicago': {
         'name': 'Chicago',
         'patterns': [
-            r'''(?P<authors>.+?\.)\s+"(?P<title>.+?)\."\s+(?P<publish>.+?)\s+\((?P<year>19|20\d{2})\)\.''',
+            r'''(?P<authors>.+?\.)\s+"(?P<title>.+?)\."\s+(?P<pub>.+?)\s+\((?P<year>19|20\d{2})\)\.''',
         ]
     },
     'APA': {
         'name': 'American Psychological Association',
         'patterns': [
-            r'''(?P<authors>.+?\.)\s+\((?P<year>19|20\d{2})\)\.\s+(?P<title>.+?)\.\s+(?P<publish>.+?)\.''',
+            r'''(?P<authors>.+?\.)\s+\((?P<year>19|20\d{2})\)\.\s+(?P<title>.+?)\.\s+(?P<pub>.+?)\.''',
         ]
     },
     'Harvard': {
         'name': 'Harvard',
         'patterns': [
-            r'''(?P<authors>.+?\.),\s+(?P<year>19|20\d{2})\.\s+(?P<title>.+?)\.\s+(?P<publish>.+?)\.''',
+            r'''(?P<authors>.+?\.),\s+(?P<year>19|20\d{2})\.\s+(?P<title>.+?)\.\s+(?P<pub>.+?)\.''',
         ]
     },
     'Vancouver': {
         'name': 'Vancouver',
         'patterns': [
-            r'''(?P<authors>.+?\.)\s+(?P<title>.+?)\.\s+(?P<publish>.+?)\.\s+(?P<year>19|20\d{2})''',
+            r'''(?P<authors>.+?\.)\s+(?P<title>.+?)\.\s+(?P<pub>.+?)\.\s+(?P<year>19|20\d{2})''',
         ]
     },
     'IEEE': {
         'name': 'IEEE',
         'patterns': [
-            r'''(?P<authors>.+?),\s+"(?P<title>.+?),"\s+(?P<publish>.+?)\.\s+(?P<year>19|20\d{2})''',
+            r'''(?P<authors>.+?),\s+"(?P<title>.+?),"\s+(?P<pub>.+?)\.\s+(?P<year>19|20\d{2})''',
         ]
     }
 }
--- refextract.py
+++ refextract.py
@@ -1,10 +1,19 @@
-from typing import Optional, Tuple
-import re
 
-from PyPDF2 import PdfFileReader, DocumentInformation
+import time
+import json
+import re
+from pprint import pprint
+from typing import Optional, Tuple, List
+
+import jinja2
 import textract
+from PyPDF2 import PdfFileReader, DocumentInformation
+from scholarly import scholarly, ProxyGenerator
 
 from citations import REF_TITLE, CITATIONS
+
+
+HTML_TEMPLATE_FILE = 'template.html'
 
 
 def extract_information(pdf_path: str) -> DocumentInformation:
@@ -24,18 +33,40 @@ def extract_information(pdf_path: str) -> DocumentInformation:
     Number of pages: {number_of_pages}
     """
 
-    print(txt)
+    print(f'\n{txt}\n')
     return information
 
 
 def extract_refs(pdf_path: str):
+    # get given pdf info
+    pdf_info = extract_information(pdf_path)
+    # extract the references
     text = textract.process(pdf_path, encoding='utf-8').decode()
     ref_index = text.find(REF_TITLE)
     if ref_index > -1:
-        refs = clean_refs(text[ref_index:])
-        print(f'\n\n{refs[:500]}')
-        cit_name, pattern = find_pattern(refs[:500])
+        text = clean_refs(text[ref_index:])
+        print(f'\n\n{text[:500]}')
+        cit_name, pattern = find_pattern(text[:500])
         print(f'\n{cit_name}\n{pattern}')
+
+        if pattern is None:
+            pass
+        else:
+            titles = get_ref_titles(text, pattern)
+            pprint(titles)
+            print()
+
+            # pg = ProxyGenerator()
+            # print(pg.FreeProxies())
+            # scholarly.use_proxy(pg)
+            pubs = query_scholars(titles)
+            data = {
+                'title': pdf_info.title,
+                'author': pdf_info.author,
+                'refs': pubs
+            }
+            print(data)
+            save_html(data, HTML_TEMPLATE_FILE, 'output.html')
 
 
 def clean_refs(refs: str) -> str:
@@ -58,8 +89,43 @@ def find_pattern(sample: str) -> Tuple[Optional[str], Optional[str]]:
     return None, None
 
 
+def get_ref_titles(text: str, pattern: str) -> List:
+    titles = []
+    for match in re.finditer(pattern, text):
+        titles.append(match.group('title'))
+
+    return titles
+
+
+def query_scholars(titles: List[str]):
+    pubs = []
+    for title in titles[:3]:
+        time.sleep(10)
+        print(f'querying "{title}" ...')
+        query = scholarly.search_pubs(title)
+        pub = next(query)  # get the first result
+        # pub = scholarly.fill(pub)
+        # pprint(scholarly.bibtex(pub))
+        #     with open('scholar_result.json', 'w') as f:
+        #         json.dump(pub, f)
+        pubs.append(pub)
+
+    return pubs
+
+
+def save_html(data: dict, template_file: str, out_file: str):
+    # get the html template
+    with open(template_file, mode='r', encoding='utf-8') as f:
+        source_html = f.read()
+
+    template = jinja2.Template(source_html)
+    rendered_html = template.render(data=data)
+    with open(out_file, mode='w', encoding='utf-8') as out:
+        out.write(rendered_html)
+    return True
+
 
 
 if __name__ == '__main__':
-    extract_information('./pdf/molgan.pdf')
+    # extract_information('./pdf/molgan.pdf')
     extract_refs('./pdf/molgan.pdf')
